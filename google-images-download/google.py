337215ed05c68cf7a51c5385f8120e462e62ac12
toswm128 <chominsu10@gmail.com>
2021-02-11 23:17:06 +0900

    사진 300개히히ㅣㅎ

--- google-images-download/google.py
+++ google-images-download/google.py
@@ -2,6 +2,6 @@ from google_images_download import google_images_download
 
 response = google_images_download.googleimagesdownload()   
 
-arguments = {"keywords":"레드벨벳 슬기,레드벨벳 예리, 레드벨벳 조이, 레드벨벳 웬디","limit":20,"print_urls":True} 
+arguments = {"keywords":"Polar bears,baloons,Beaches","limit":20,"print_urls":True} 
 paths = response.download(arguments)   
 print(paths)  
